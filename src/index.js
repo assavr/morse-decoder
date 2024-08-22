a7a03996c9f01f4f3f3403b021419740997d81a0
Anastasia Savrukhina <assavr0@gmail.com>
2024-08-22 18:09:51 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -35,11 +35,20 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' '
 };
 
 function decode(expr) {
-    // write your solution here
+    const amountLetters = expr.length / 10;
+    const arrsLettersNum = new Array(amountLetters).fill([]).map((letterInNum, i) => expr.slice(i*10, (i + 1) * 10))
+    const morseLetters = arrsLettersNum.map((letter) => {
+        return letter.replace(/10/g,'.').replace(/11/g, '-').replace(/0/g,'')
+        })
+    const res = morseLetters.map((letter) => MORSE_TABLE[letter]).join('')
+    return res
 }
+
+decode('00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010')
 
 module.exports = {
     decode
